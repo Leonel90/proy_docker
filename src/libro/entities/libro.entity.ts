6818bd30d8b64443a4f2e68cce96ac9b30431c34
unknown <sleonel1990@gmail.com>
2023-11-22 08:54:17 -0500

    Creación de entidad Libro

--- src/libro/entities/libro.entity.ts
+++ src/libro/entities/libro.entity.ts
@@ -1,1 +1,25 @@
-export class Libro {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Libro {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column(
+        'text'
+    )
+    nombre: string;
+
+    @Column({
+        type: 'text',
+        nullable: true // persmite vacios
+    })
+    edicion: string;
+
+    @Column('int',{
+        default: 0,
+        nullable: false
+    })
+    anio: number;
+
+}
